b52dbe305f3b93d024c98e77ffb1b21527cc29bf
Christoffer <nordin.christoffer@gmail.com>
2025-08-25 14:32:09 +0200

    Allow PokeAPI images

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  //
   images: {
     remotePatterns: [
-      { hostname: "pokeapi.com" },
+      new URL('https://pokeapi.co/**'),
+      new URL('https://raw.githubusercontent.com/**')
     ],
   },
 };
